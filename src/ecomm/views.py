b58ce82e9c76dde71a9fbf3f4c3ace5a982f6676
Rajareddy Dodda <rajareddydodda@Rajareddys-MacBook-Pro.local>
2019-07-28 20:39:50 +0100

    products
    new project products created

--- src/ecomm/views.py
+++ src/ecomm/views.py
@@ -2,4 +2,19 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 def home_page(request):
-    return render(request, "home_page.html", {})
+    context = {
+        "title":"Hello world title"
+    }
+    return render(request, "home_page.html", context)
+
+def about_page(request):
+    context = {
+        "title":"About Page"
+    }
+    return render(request, "home_page.html", context)
+
+def contact_page(request):
+    context = {
+        "title":"contact  Page"
+    }
+    return render(request, "home_page.html", context)
